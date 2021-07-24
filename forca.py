63cdcbf401d84cae70feb764be69923323403a2b
Carlos <carlos_rj18@hotmail.com>
2021-07-24 00:09:37 -0300

    melhorando o código

--- forca.py
+++ forca.py
@@ -8,19 +8,17 @@ def jogar():
     enforcou = False
     acertou = False
 
-    while(not acertou and not enforcou):
+    while (not acertou and not enforcou):
 
         chute = input("Qual letra? ")
+        chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if (chute == letra):
-                print("Encontrei a letra {} na posição {}".format(letra , index))
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
-
-        print("Jogando...")
-
-    print("Fim do jogo")
+    print("Jogando...")
 
 if(__name__ == "__main__"):
     jogar()
